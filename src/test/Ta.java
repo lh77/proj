76130a82702728a45c085947362b3b35c0ea1a72
zerow7 <790131821@qq.com>
2017-06-09 15:55:22 +0800

    Create Ta.java

--- src/test/Ta.java
+++ src/test/Ta.java
@@ -4,7 +4,9 @@ public class Ta {
 	public static void print(int x){
 		
 	}
-	public int sum(int x){}
+	public int sum(int x){
+		return x;
+	}
 	public static void main(String[] args) {
 		print(2);
 	}
